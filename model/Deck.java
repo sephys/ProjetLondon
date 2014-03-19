0cdaf122f457e8756b9966239d93c7cc46bb6cc4
Anh-Djuy <anhdjuy@gmail.com>
2014-03-19 15:39:25 +0100

    Ajout du Deck

---
+++ model/Deck.java
@@ -0,0 +1,220 @@
+package model;
+
+import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
+
+public class Deck implements Deque<Carte>{
+
+	@Override
+	public boolean addAll(Collection<? extends Carte> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(Carte e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void addFirst(Carte e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addLast(Carte e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator<Carte> descendingIterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Carte element() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Carte getFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Carte getLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<Carte> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean offer(Carte e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean offerFirst(Carte e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean offerLast(Carte e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Carte peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Carte peekFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Carte peekLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Carte poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Carte pollFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Carte pollLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Carte pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void push(Carte e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Carte remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Carte removeFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeFirstOccurrence(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Carte removeLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeLastOccurrence(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+}
