44ba202e006031e1a8eaf205510c3eae983139a8
sephys <baptistemulot88@gmail.com>
2014-03-20 20:31:19 +0100

    Ajout de toute les cartes contructible, Modification fichier test Pour lecture du fichier Carte.xls et creation de tout les cartes

--- model/test.java
+++ model/test.java
@@ -23,22 +23,28 @@ public class test {
 			Sheet sheet = workbook.getSheet(0);
 			Cell a1,a2,a3;
 			ArrayList<Carte> test = new ArrayList<Carte>();
-			for(int i=2;i<21;i++){
-				test.add(new Constructible(
-						sheet.getCell(0,i).getContents(),
-						sheet.getCell(1,i).getContents(),
-						sheet.getCell(2,i).getContents(),
-						Integer.parseInt(sheet.getCell(3,i).getContents()),
-						new String[]{sheet.getCell(5,i).getContents(),sheet.getCell(6,i).getContents()},
-						Integer.parseInt(sheet.getCell(4,i).getContents()),
-						new int[]{Integer.parseInt(sheet.getCell(7,i).getContents()),Integer.parseInt(sheet.getCell(8,i).getContents()),Integer.parseInt(sheet.getCell(9,i).getContents())},
-						sheet.getCell(10,i).getContents(),
-						sheet.getCell(11,i).getContents(),
-						Boolean.parseBoolean(sheet.getCell(12,i).getContents()),
-						Boolean.parseBoolean(sheet.getCell(13,i).getContents())
-						));
-				System.out.println(test.get(i-2));
+			int compteurCarte=0;
+			for(int i=2;i<72;i++){
+				int nb=Integer.parseInt(sheet.getCell(14,i).getContents());
+				for (int j=1;j<=nb;j++){
+					test.add(new Constructible(
+								sheet.getCell(0,i).getContents(),
+								sheet.getCell(1,i).getContents(),
+								sheet.getCell(2,i).getContents(),
+								Integer.parseInt(sheet.getCell(3,i).getContents()),
+								new String[]{sheet.getCell(5,i).getContents(),sheet.getCell(6,i).getContents()},
+								Integer.parseInt(sheet.getCell(4,i).getContents()),
+								new int[]{Integer.parseInt(sheet.getCell(7,i).getContents()),Integer.parseInt(sheet.getCell(8,i).getContents()),Integer.parseInt(sheet.getCell(9,i).getContents())},
+								sheet.getCell(10,i).getContents(),
+								sheet.getCell(11,i).getContents(),
+								Boolean.parseBoolean(sheet.getCell(12,i).getContents()),
+								Boolean.parseBoolean(sheet.getCell(13,i).getContents())
+								));
+					compteurCarte++;
+					System.out.println(test);
+				}
 			}
+			System.out.println(compteurCarte);
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
