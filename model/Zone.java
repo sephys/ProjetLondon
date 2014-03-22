858b5bcd61d26b6c1e214147c159cac93ddea368
jean <jean.wisser@gmail.com>
2014-03-22 16:46:20 +0100

    Modification classe Zone

--- model/Zone.java
+++ model/Zone.java
@@ -16,4 +16,44 @@ public class Zone {
     private int prix;
     private int nbCartes;
     private int pointsVictoire;
+
+    public Zone(String nom, int prix, int nbCartes, int pointsVictoire) {
+        this.nom = nom;
+        this.prix = prix;
+        this.nbCartes = nbCartes;
+        this.pointsVictoire = pointsVictoire;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public int getNbCartes() {
+        return nbCartes;
+    }
+
+    public void setNbCartes(int nbCartes) {
+        this.nbCartes = nbCartes;
+    }
+
+    public int getPointsVictoire() {
+        return pointsVictoire;
+    }
+
+    public void setPointsVictoire(int pointsVictoire) {
+        this.pointsVictoire = pointsVictoire;
+    }
+    
 }
