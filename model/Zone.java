489eb19ca053164c3e76b169f482b19b77aa7d7a
jean <jean.wisser@gmail.com>
2014-03-22 17:45:41 +0100

    mise a jour classe Zone 2

--- model/Zone.java
+++ model/Zone.java
@@ -6,6 +6,7 @@
 
 package model;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -22,14 +23,92 @@ public class Zone {
     private boolean zoneRouge;
     private boolean adjacentTamise;
     private boolean dessousTamise;
-    Set<Zone> zonesAdjacentes =  new HashSet<>() ;
-    
+    private Set<Zone> zonesAdjacentes;
 
-    public Zone(String nom, int prix, int nbCartes, int pointsVictoire) {
+    
+    public Zone(String nom, int prix, int nbCartes, int pointsVictoire, boolean zoneRouge, boolean adjacentTamise, boolean dessousTamise, String[] zonesAdjacentes) {
         this.nom = nom;
         this.prix = prix;
         this.nbCartes = nbCartes;
         this.pointsVictoire = pointsVictoire;
+        this.zoneRouge = zoneRouge;
+        this.adjacentTamise = adjacentTamise;
+        this.dessousTamise = dessousTamise;
+        this.zonesAdjacentes = new HashSet(Arrays.asList(zonesAdjacentes));
     }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public int getNbCartes() {
+        return nbCartes;
+    }
+
+    public void setNbCartes(int nbCartes) {
+        this.nbCartes = nbCartes;
+    }
+
+    public int getPointsVictoire() {
+        return pointsVictoire;
+    }
+
+    public void setPointsVictoire(int pointsVictoire) {
+        this.pointsVictoire = pointsVictoire;
+    }
+
+    public boolean isZoneRouge() {
+        return zoneRouge;
+    }
+
+    public void setZoneRouge(boolean zoneRouge) {
+        this.zoneRouge = zoneRouge;
+    }
+
+    public boolean isAdjacentTamise() {
+        return adjacentTamise;
+    }
+
+    public void setAdjacentTamise(boolean adjacentTamise) {
+        this.adjacentTamise = adjacentTamise;
+    }
+
+    public boolean isDessousTamise() {
+        return dessousTamise;
+    }
+
+    public void setDessousTamise(boolean dessousTamise) {
+        this.dessousTamise = dessousTamise;
+    }
+
+    public Set<Zone> getZonesAdjacentes() {
+        return zonesAdjacentes;
+    }
+
+    public void setZonesAdjacentes(Set<Zone> zonesAdjacentes) {
+        this.zonesAdjacentes = zonesAdjacentes;
+    }
+    
+    @Override
+    public String toString(){
+        return this.nom+" [prix : "+this.prix+", nbCartes : "+this.nbCartes+", pointsVictoire : "+this.pointsVictoire+", ";
+    }
+
+
+    
+
+
     
 }
--- model/test.java
+++ model/test.java
@@ -4,14 +4,21 @@ package model;
 public class test {
 
 	public static void main(String[] args) {
+            
 		// TODO Auto-generated method stub
-		//création du deck
+		//crï¿½ation du deck
 		Deck d=new Deck();
 		for(Carte a: d){
 			System.out.println("Categorie : "+a.getCategorie()+" Nom : "+a.getNom());
 		}
-		//mélange du deck;
+		//mï¿½lange du deck;
 		System.out.println(d.size());
+                
+                                //Initialisation des zones
+                String zonesAdjacentes[] = { "Hackey", "Bethnal Green", "Southwark", "St. Pancras", "St. Marylebone", "Islington" };
+                Zone z = new Zone("City", 8, 4, 6, true, true, false, zonesAdjacentes);
+                System.out.println("Zone :");
+                System.out.println(z);
 	}
 	
 
