cd9a1f84a13a643ba95e4e771d04ccd53f767461
Baptiste <baptistemulot88@gmail.com>
2014-04-06 17:41:46 +0200

    modification london

--- vue/London.java
+++ vue/London.java
@@ -32,7 +32,6 @@ public class London {
 	private static TourJoueur lJoueur;
 	private static Deck deck;
 	private static Etalage etalage;
-<<<<<<< HEAD
 	private static JPMain[] tabJPMain;
 
 	static JFrame frame; // fenêtre principale
@@ -94,8 +93,9 @@ public class London {
 		MenuDroite menudroite=new MenuDroite();
 
 		frame.add(menudroite,BorderLayout.EAST);
+		frame.add(new JPInfos(),BorderLayout.WEST);
 
-		frame.setSize(1114,810);
+		frame.setSize(1444,810);
 
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
@@ -103,156 +103,60 @@ public class London {
 		System.out.println(deck);
 		System.out.println(etalage);
 		System.out.println(plateau);
-=======
-	private static Plateau plateau;
-        private static JPMain[] tabJPMain;
-    
-    static JFrame frame; // fenêtre principale
-    
-    // pour le drag & drop
-    static DragDrop dndListener;
-    static DragSource dragSource;
-    static JFrame acc;
-    public static void main(String[] args)  {
-       
-       // D&D 
-       dndListener = new DragDrop();
-       dragSource=new DragSource();
-       
-        menu();   
-    }
-    
-    // méthode qui initialise la fenêtre lorsqu'on lance une partie
-    public static void start()
-    {
-           
-           acc.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-           acc.dispose();
-           frame=new JFrame();
-          
-           frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            JTabbedPane panelOnglet = new JTabbedPane();
-            
-            JPPlateau plateau=null;
-            try
-            {
-               URL uri = London.class.getResource("../img/plateau.png");
-               Image image = ImageIO.read(uri);
-               plateau = new JPPlateau(image);
-              
-            }
-            catch (IOException e1) {
-            }
-            
-            
-            
-            // ajout d'un onglet :
-            panelOnglet.addTab("Plateau",new JScrollPane(plateau));
-
-            panelOnglet.addTab("Etalage",new JPEtalage());
-
-          
- 
-            JPanel p=new JPanel(new BorderLayout());
-            JPanel south=new JPMain(Joueur.getTabJoueur()[0]);
-            south.setBackground(Color.blue);
-            //south.setPreferredSize(new Dimension(1000,150));
-            //south.add(new JPMain());
-            p.add(panelOnglet,BorderLayout.CENTER);
-            p.add(south,BorderLayout.SOUTH);
-            
-            frame.add(p);
-            
-            MenuDroite menudroite=new MenuDroite();
-            
-            frame.add(menudroite,BorderLayout.EAST);
-            frame.add(new JPInfos(),BorderLayout.WEST);
- 
-            frame.setSize(1444,810);
-            
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
-            System.out.println(lJoueur);
-            System.out.println(deck);
-            System.out.println(etalage);
-            System.out.println(plateau);
-            
-    }
-    
-    // methode qui affiche le menu quand on lance l'application
-    public static void menu()
-    {
-        acc=new JFrame();
-  
-        acc.setTitle("London");
-        acc.setSize(570,810);
-        acc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        acc.setLayout(new BorderLayout());
-      
-        acc.setContentPane(new JPAccueil());
-
-       
-        acc.setLocationRelativeTo(null);
-        acc.setVisible(true);
-       
-    }
->>>>>>> d086def0617f14be2e60a7c1e016fdf95515fd53
 
 	}
+// methode qui affiche le menu quand on lance l'application
+public static void menu()
+{
+	acc=new JFrame();
 
-	// methode qui affiche le menu quand on lance l'application
-	public static void menu()
+	acc.setTitle("London");
+	acc.setSize(570,810);
+	acc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	acc.setLayout(new BorderLayout());
+
+	acc.setContentPane(new JPAccueil());
+
+
+	acc.setLocationRelativeTo(null);
+	acc.setVisible(true);
+
+}
+
+public static void setDeck(Deck deck2) {
+	// TODO Auto-generated method stub
+	deck=deck2;
+}
+
+public static Deck getDeck() {
+	// TODO Auto-generated method stub
+	return deck;
+}
+
+public static TourJoueur getListeJoueur() {
+	// TODO Auto-generated method stub
+	return lJoueur;
+}
+
+public static void setListeJoueur(TourJoueur initialisationJoueur) {
+	// TODO Auto-generated method stub
+	lJoueur=initialisationJoueur;
+}
+
+public static void setEtalage(Etalage etalage2) {
+	// TODO Auto-generated method stub
+	etalage=etalage2;
+}
+
+public void initTabJPMain()
+{
+	tabJPMain=new JPMain[Joueur.getNbJoueur()];
+	for(int i=0;i<Joueur.getNbJoueur();i++)
 	{
-		acc=new JFrame();
-
-		acc.setTitle("London");
-		acc.setSize(570,810);
-		acc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		acc.setLayout(new BorderLayout());
-
-		acc.setContentPane(new JPAccueil());
-
-
-		acc.setLocationRelativeTo(null);
-		acc.setVisible(true);
-
+		tabJPMain[i]=new JPMain(Joueur.getTabJoueur()[i]);
 	}
-
-	public static void setDeck(Deck deck2) {
-		// TODO Auto-generated method stub
-		deck=deck2;
-	}
-
-	public static Deck getDeck() {
-		// TODO Auto-generated method stub
-		return deck;
-	}
-
-	public static TourJoueur getListeJoueur() {
-		// TODO Auto-generated method stub
-		return lJoueur;
-	}
-
-	public static void setListeJoueur(TourJoueur initialisationJoueur) {
-		// TODO Auto-generated method stub
-		lJoueur=initialisationJoueur;
-	}
-
-	public static void setEtalage(Etalage etalage2) {
-		// TODO Auto-generated method stub
-		etalage=etalage2;
-	}
-
-	public void initTabJPMain()
-	{
-		tabJPMain=new JPMain[Joueur.getNbJoueur()];
-		for(int i=0;i<Joueur.getNbJoueur();i++)
-		{
-			tabJPMain[i]=new JPMain(Joueur.getTabJoueur()[i]);
-		}
-	}
+}
 
 }
 
