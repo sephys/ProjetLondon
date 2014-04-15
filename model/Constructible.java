6a6675476516ddd182060c16eeb3317645a897e3
Baptiste <baptistemulot88@gmail.com>
2014-04-15 17:19:46 +0200

    correction bug

--- model/Constructible.java
+++ model/Constructible.java
@@ -119,12 +119,18 @@ public class Constructible extends Carte {
 	public void jouerCarte(Joueur currJ,int ind) { 
 		// TODO Auto-generated method stub
 		String pouv = this.getPouvoirIlli();
-		if(pouv!=null){
+		if(pouv!=null&&currJ.getPouvoir().get(pouv)!=null){
 			currJ.getPouvoir().put(pouv,new Integer(currJ.getPouvoir().get(pouv).intValue()+1));
+		}else{
+			currJ.getPouvoir().put(pouv,new Integer(1));
 		}
 		currJ.setArgent(currJ.getArgent()-this.getCoutPose());
 		currJ.setPointVictoire(currJ.getPointVictoire()+this.getPointsVictoirePose());
 		currJ.getListeChantier().get(ind).add(this);
 		currJ.getMain().remove(this);
+	}
+	
+	public void activerCarte(Joueur currJ){
+		
 	}
 }
