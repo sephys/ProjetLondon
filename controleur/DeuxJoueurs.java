7e7d698cd948e54dc76b5898774299f518069af4
Anh-Djuy <anhdjuy@gmail.com>
2014-04-21 20:17:47 +0200

    refactorisation de JPAccueil

---
+++ controleur/DeuxJoueurs.java
@@ -0,0 +1,26 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+
+package controleur;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import model.Joueur;
+
+/**
+ *
+ * @author Anh-Djuy
+ */
+public class DeuxJoueurs implements ActionListener{
+    
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Joueur.setNbJoueur(2);
+    }
+    
+}
---
+++ controleur/QuatreJoueurs.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package controleur;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import model.Joueur;
+
+/**
+ *
+ * @author Anh-Djuy
+ */
+public class QuatreJoueurs implements ActionListener {
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Joueur.setNbJoueur(3);
+    }
+}
