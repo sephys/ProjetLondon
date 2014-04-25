bfb42a155687bd5cf530e766367e322fa4c076cf
Baptiste <baptistemulot88@gmail.com>
2014-04-25 14:54:56 +0200

    peutJouer

--- model/PouvoirBeta.java
+++ model/PouvoirBeta.java
@@ -220,5 +220,18 @@ public class PouvoirBeta {
             }
         }
     }
-
+    
+    //contrôle
+    public static boolean peutJouer(Joueur j,Carte ca){
+    	boolean res=false;
+    	if(j.getPouvoir().get("Wren")==1){
+    		res=true;
+    	}
+    	if(ca.getNom().compareTo("School")==0||ca.getNom().compareTo("Coffee")==0){
+    		res=true;
+    	}
+    	return res;
+    	
+    }
+    
 }
